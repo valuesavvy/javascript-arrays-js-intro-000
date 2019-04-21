9a4a330709fd8083858c08149ee5665fac533ed3
James Powell <nvrquitjp@gmail.com>
2019-04-21 04:31:44 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ];
+  
+  
+ var newFart = addElementToBeginningOfArray.unshift("moose")
+  
+  
+
+  var destructivelyAddElementToBeginningOfArray = ["foot", "hand", "mouth"];
+  destructivelyAddElementToBeginningOfArray.push("forehead");
+  return destructivelyAddElementToBeginningOfArray
